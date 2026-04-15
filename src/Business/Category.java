01d31645889f2a84a0bfdac6d2d14d66c935aa23
Ekutepev <evgeny.kutepov@gmail.com>
2026-04-15 09:03:18 -0600

    update

--- src/Business/Category.java
+++ src/Business/Category.java
@@ -3,32 +3,5 @@ package Business;
 import Business.Interfaces.ICategory;
 
 public class Category implements ICategory {
-    private int id;
-    private String name;
-
-    public Category(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- src/Business/Rental.java
+++ src/Business/Rental.java
@@ -1,57 +1,5 @@
 package Business;
 
 public class Rental {
-    private int rentalId;
-    private int customerId;
-    private int equipmentId;
-    private String rentalDate;
-    private String returnDate;
-
-    public Rental(int rentalId, int customerId, int equipmentId, String rentalDate, String returnDate) {
-        this.rentalId = rentalId;
-        this.customerId = customerId;
-        this.equipmentId = equipmentId;
-        this.rentalDate = rentalDate;
-        this.returnDate = returnDate;
-    }
-
-    public int getRentalId() {
-        return rentalId;
-    }
-
-    public void setRentalId(int rentalId) {
-        this.rentalId = rentalId;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-
-    public int getEquipmentId() {
-        return equipmentId;
-    }
-
-    public void setEquipmentId(int equipmentId) {
-        this.equipmentId = equipmentId;
-    }
-
-    public String getRentalDate() {
-        return rentalDate;
-    }
-
-    public void setRentalDate(String rentalDate) {
-        this.rentalDate = rentalDate;
-    }
-
-    public String getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(String returnDate) {
-        this.returnDate = returnDate;
-    }
+    
 }
