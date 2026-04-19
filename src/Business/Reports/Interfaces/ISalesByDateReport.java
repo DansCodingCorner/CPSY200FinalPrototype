5e5767ebee164732ca3b50e3e4a021bd8784cb0c
Daniel <danielcaron1995@gmail.com>
2026-04-19 11:25:09 -0700

    Created Sales By Date report and fixed minor bugs

--- src/Business/Reports/Interfaces/ISalesByDateReport.java
+++ src/Business/Reports/Interfaces/ISalesByDateReport.java
@@ -1,6 +1,10 @@
 package Business.Reports.Interfaces;
 
+import java.time.LocalDate;
+
+import Persistence.IRentalDataAccess;
+
 public interface ISalesByDateReport 
 {
-
+	public String generateReport(IRentalDataAccess dataAccess, LocalDate date);
 }
